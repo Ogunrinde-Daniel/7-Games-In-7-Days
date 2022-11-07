21c17399ccbb8471e0f83f0a8531cd515a5e067e
Oluwaseun770 <98650324+Oluwaseun770@users.noreply.github.com>
2022-11-06 20:17:25 -0400

    Version 1.1 of built games DAY 1

--- 2_Rain_Collector/Assets/Scripts/collectible.cs
+++ 2_Rain_Collector/Assets/Scripts/collectible.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class collectible : MonoBehaviour
+public class Collectible : MonoBehaviour
 {
 
     string type = "unknown";
     int speed = Random.Range(20,50);
+    public Rigidbody2D rb;
     
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.position = randomPosition();
     }
 
     // Update is called once per frame
@@ -19,14 +21,16 @@ public class collectible : MonoBehaviour
         
     }
 
-    void randomPosition()
+    Vector2 randomPosition()
     {
         float[] targetSize = {0,0 };
         Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
 
-        float minX = -bounds.x + targetSize[0] / 2f;
-        float maxX =  bounds.x - targetSize[0] / 2f;
-        float minY = -bounds.y + targetSize[1] / 2f;
-        float maxY =  bounds.y - targetSize[1] / 2f;
+        float minX = bounds.x + targetSize[0] / 2f;
+        float maxX = minX + targetSize[0];
+        float minY = bounds.y + targetSize[1] / 2f;
+        float maxY = minY + targetSize[1];
+
+        return new Vector2(Random.Range(minX,maxX), Random.Range(minY, maxY));
     }
 }
